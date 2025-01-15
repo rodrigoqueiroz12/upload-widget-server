4c2b809bc0001440f53c769120118c41a397bfde
Rodrigo Rocha de Queiroz <rodrigo.queiroz0629@gmail.com>
2025-01-15 20:29:26 -0300

    image upload function

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,5 +1,4 @@
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
+import { uploadImage } from '@/app/functions/upload-image'
 import type { FastifyInstance } from 'fastify'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -12,10 +11,7 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async (
 		{
 			schema: {
 				summary: 'Upload an image',
-				body: z.object({
-					name: z.string(),
-					password: z.string().optional()
-				}),
+				consumes: ['multipart/form-data'],
 				response: {
 					201: z.object({ uploadId: z.string() }),
 					409: z
@@ -24,11 +20,21 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async (
 				}
 			}
 		},
-		async (_, reply) => {
-			await db.insert(schema.uploads).values({
-				name: 'test.jpg',
-				remoteKey: 'test.jpg',
-				remoteUrl: 'http://asdasd.com'
+		async (request, reply) => {
+			const uploadedFile = await request.file({
+				limits: {
+					fieldSize: 1024 * 1024 * 2 // 2mb
+				}
+			})
+
+			if (!uploadedFile) {
+				return reply.status(400).send({ message: 'File is required.' })
+			}
+
+			await uploadImage({
+				fileName: uploadedFile.filename,
+				contentType: uploadedFile.mimetype,
+				contentStream: uploadedFile.file
 			})
 
 			return reply.status(201).send({ uploadId: '' })
