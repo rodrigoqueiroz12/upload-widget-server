e21c6bf4b3adc4fbecae73b01652b7d447652c17
Rodrigo Rocha de Queiroz <rodrigo.queiroz0629@gmail.com>
2025-01-14 21:07:35 -0300

    drizzle setup

---
+++ src/infra/db/migrations/0000_large_jackpot.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "uploads" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"remote_key" text NOT NULL,
+	"remote_url" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
