3ec95e6df704c390edc8403552edcdf54d84ce43
Rodrigo Rocha de Queiroz <rodrigo.queiroz0629@gmail.com>
2025-01-16 20:55:24 -0300

    setup cloudflare r2

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,13 @@ import { z } from 'zod'
 const envSchema = z.object({
 	APP_PORT: z.coerce.number().default(3333),
 	NODE_ENV: z.enum(['production', 'test', 'development']).default('production'),
-	DATABASE_URL: z.string().url().startsWith('postgresql://')
+	DATABASE_URL: z.string().url().startsWith('postgresql://'),
+
+	CLOUDFLARE_BUCKET: z.string(),
+	CLOUDFLARE_PUBLIC_URL: z.string().url(),
+	CLOUDFLARE_ACCOUNT_ID: z.string(),
+	CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+	CLOUDFLARE_SECRET_ACCESS_KEY_ID: z.string()
 })
 
 export type Env = z.infer<typeof envSchema>
